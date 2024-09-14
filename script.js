95fc2947e0a429ae3779ee5a64af4810f1ff79d8
Cat-logo-in-github <parshatwork@gmail.com>
2024-09-14 18:39:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ball = document.getElementById('ball');
     ball.addEventListener('animationend', () => {
-        ball.style.display = 'none';
         document.getElementById('content').classList.remove('hidden');
     });
 });
